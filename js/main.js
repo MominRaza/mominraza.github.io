822ff75049881714a41749f82d944733ef206635
MominRaza <mominr082@gmail.com>
2020-10-07 17:24:06 +0530

    added mayro to portfolio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 var navButton = document.getElementById('nav-button');
 function toggleMenu() {
-    if (navButton.classList.contains('active')) {
-        navButton.classList.remove('active');
-    } else {
-        navButton.classList.add('active');
-    }
+    navButton.classList.toggle('active');
 }
